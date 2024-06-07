1898ccb165b50ad26a291743990419c627295ab5
Ajay Kumar Yadav <ajay.yadav1@tothenew.com>
2024-06-07 11:55:00 +0530

    JS: added removeDownloadVideo cacheVideoModule

--- Video.js
+++ Video.js
@@ -15,13 +15,13 @@ const styles = StyleSheet.create({
 });
 
 const { VideoDecoderProperties,CacheVideoModule,DownloadEventEmitter } = NativeModules
-const {downloadVideoUsingUri,isVideoAvailableForOffline,removeAllDownloads,restorePersistenceManager} =CacheVideoModule;
+const {downloadVideoUsingUri,isVideoAvailableForOffline,removeAllDownloads,restorePersistenceManager, removeDownloadVideo} =CacheVideoModule;
 let downloadManagerEmitter;
 if(Platform.OS ==='android')
  downloadManagerEmitter = new NativeEventEmitter(CacheVideoModule);
  else
  downloadManagerEmitter = new NativeEventEmitter(DownloadEventEmitter);
- export { TextTrackType, FilterType, DRMType, VideoDecoderProperties,downloadVideoUsingUri,isVideoAvailableForOffline,downloadManagerEmitter ,removeAllDownloads,restorePersistenceManager}
+ export { TextTrackType, FilterType, DRMType, VideoDecoderProperties,downloadVideoUsingUri,isVideoAvailableForOffline,downloadManagerEmitter ,removeAllDownloads,restorePersistenceManager, removeDownloadVideo}
 
 export default class Video extends Component {
 
